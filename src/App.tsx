c5d90d53ff6152a9b3eebce41c7b2fa061bb87c8
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-10-04 11:55:35 +0700

    add mui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+
+type TQuestion = {
+  question: string;
+  rule: "MAY_SELECT" | "MUST_SELECT";
+  optionAnswers: Array<String>;
+};
 
 function App() {
+  const [inputQuestion, setInputQuestion] = useState<TQuestion>({
+    question: "",
+    rule: "MAY_SELECT",
+    optionAnswers: [],
+  });
+  const [inputOptionAnswer, setInputOptionAnswer] = useState("");
+
+  const handleAddOptionAnswer = () => {
+    if (!inputOptionAnswer) return;
+    setInputQuestion({
+      ...inputQuestion,
+      optionAnswers: [...inputQuestion.optionAnswers, inputOptionAnswer],
+    });
+  };
+
+  const handleAddQuestion = () => {
+    const questions = JSON.parse(localStorage.getItem("questions"));
+
+    localStorage.setItem("questions", JSON.stringify(items));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <TextField label='Question' variant='outlined' />
+      <FormControl fullWidth>
+        <InputLabel id='demo-simple-select-label'>Rule</InputLabel>
+        <Select
+          labelId='demo-simple-select-label'
+          id='demo-simple-select'
+          value={inputQuestion.rule}
+          label='Age'
+          // onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MenuItem value={"MAY_SELECT"}>May Select</MenuItem>
+          <MenuItem value={"MUST_SELECT"}>Must Select</MenuItem>
+        </Select>
+      </FormControl>
+      <Box>
+        <TextField
+          onChange={(e) => setInputOptionAnswer(e.target.value)}
+          label='Answer option'
+          variant='outlined'
+        />
+        <Button variant='contained' onClick={() => handleAddOptionAnswer()}>
+          Add Option
+        </Button>
+      </Box>
+      <Button variant='contained'>Submit Question</Button>
+    </Container>
   );
 }
 
